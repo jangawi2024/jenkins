9068568c644a4264c9a8a1313f233e5e0f62dd0d
Leandro <leandro@Mac-mini-de-Leandro.local>
2025-06-04 16:45:15 -0300

    ajuste disco 2

--- main.tf
+++ main.tf
@@ -159,7 +159,7 @@ resource "aws_lb_target_group" "jenkins_tg" {
 
 resource "aws_lb_listener" "jenkins_listener" {
     load_balancer_arn = aws_lb.jenkins_alb.arn
-    port              = 80
+    port              = 8080
     protocol          = "HTTP"
 
     default_action {
