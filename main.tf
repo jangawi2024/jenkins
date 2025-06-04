15613307d8e098753ac366defda4ec290796ac02
Leandro <leandro@Mac-mini-de-Leandro.local>
2025-06-04 15:45:20 -0300

    ajuste disco 2

--- main.tf
+++ main.tf
@@ -111,7 +111,7 @@ resource "aws_instance" "jenkins_server" {
 
                                 # Clonar repositório com Dockerfile e docker-compose.yml
                                 sudo yum install -y git
-                                git clone https://github.com/jangawi2024/jenkins.git /mnt/dados/jenkins
+                                git clone https://github.com/jangawi2024/jenkins.git /mnt/data/jenkins
                                 cd /mnt/dados/jenkins
 
                                 # Usar o disco para Jenkins
